9a0699a8bcd4db039b4c96607f788faafb4a59d7
yamapi06150 <yamapi06150@gmail.com>
2017-05-15 21:32:46 +0800

    config deploy info.

--- src/utils/cronTools.js
+++ src/utils/cronTools.js
@@ -1,5 +1,6 @@
 const CronJob = require('cron').CronJob;
 const getYoubikeData = require('./getRemoteData/tools').getYoubikeData ;
+const fetch = require('node-fetch');
 
 const cronYoubikeData = () => {
     // console.log('cron job');
@@ -9,6 +10,19 @@ const cronYoubikeData = () => {
     }, null, true);
 } ;
 
+const cronHerokuAvoidIdleEvery5Min = async () => {
+    new CronJob('*/5 * * * *', () => {
+        console.log( new Date() );
+        try{
+            const httpResponse = await fetch('https://find-near-me.herokuapp.com/v1/ubike-station/taipei?lat=25.015777&lng=121.565478');
+            console.log(await httpResponse.json());
+        }catch(e){
+            console.log(`something wrong in cron heroku every 5 min...： ${e}`);
+        }
+    }, null, true);
+} ;
+
 module.exports = {
-    cronYoubikeData
+    cronYoubikeData,
+    cronHerokuAvoidIdleEvery5Min
 } ;
